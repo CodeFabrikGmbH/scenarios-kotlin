8445f01769facf1587f750d59a454ff82849488f
Dimitri Schrul <dimitri.schrul@gmail.com>
2021-03-18 07:34:40 +0100

    add test for creating supervisor

--- src/main/kotlin/example/application/EmployeeService.kt
+++ src/main/kotlin/example/application/EmployeeService.kt
@@ -2,6 +2,7 @@ package example.application
 
 import example.domain.model.employee.Employee
 import example.domain.model.employee.EmployeeRepository
+import example.domain.model.employee.Role
 import java.util.*
 
 class EmployeeService(
@@ -10,10 +11,11 @@ class EmployeeService(
 ) {
     fun create(
             name: String,
-            organizationId: UUID
+            organizationId: UUID,
+            role: Role
     ): Employee {
         organizationService.checkIfOrganizationExists(organizationId)
-        return employeeRepository.save(Employee(name, organizationId))
+        return employeeRepository.save(Employee(name, organizationId, role))
     }
 
     fun list(): List<Employee> {
