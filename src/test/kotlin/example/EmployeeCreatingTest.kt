3554c1269e15d2c0a5c917f43a1060154be3f60c
Dimitri Schrul <dimitri.schrul@gmail.com>
2021-03-18 09:57:28 +0100

    add test for creating an employee in a non existing organization

--- src/test/kotlin/example/EmployeeCreatingTest.kt
+++ src/test/kotlin/example/EmployeeCreatingTest.kt
@@ -1,11 +1,15 @@
 package example
 
+import com.codefabrik.scenarios.Scenario
 import example.setup.`test organization`
 import com.codefabrik.scenarios.given
 import example.domain.model.employee.Role
 import example.setup.`then the employee should exist in the test organization`
+import example.setup.`when the admin creates an employee with organization`
 import example.setup.`when the admin creates an employee`
 import org.junit.Test
+import java.lang.RuntimeException
+import java.util.*
 
 class EmployeeCreatingTest: BasicTest() {
     @Test
@@ -21,6 +25,13 @@ class EmployeeCreatingTest: BasicTest() {
         given(::`test organization`) {
             `when the admin creates an employee`("Test Employee", Role.SUPERVISOR)
             `then the employee should exist in the test organization`("Test Employee", Role.SUPERVISOR)
+        }
+    }
+
+    @Test
+    fun `creating an employee should not work if the organization does not exist`() {
+        given(::Scenario, expected = RuntimeException::class) {
+            `when the admin creates an employee with organization`("Test Employee", UUID.randomUUID(), Role.EMPLOYEE)
         }
     }
 }
--- src/test/kotlin/example/setup/When.kt
+++ src/test/kotlin/example/setup/When.kt
@@ -3,9 +3,14 @@ package example.setup
 import com.codefabrik.scenarios.Scenario
 import example.TestApplication
 import example.domain.model.employee.Role
+import java.util.*
 
 fun Scenario.`when the admin creates an organization`(organizationName: String) {
     TestApplication.organizationService.create(organizationName)
+}
+
+fun Scenario.`when the admin creates an employee with organization`(employeeName: String, organizationId: UUID, role: Role) {
+    TestApplication.employeeService.create(employeeName, organizationId, role)
 }
 
 fun `test organization`.`when the admin creates an employee`(employeeName: String, role: Role) {
