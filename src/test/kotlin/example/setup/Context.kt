ff07eeea30654db7fbebbb7463c682f3cd5d9836
Dimitri Schrul <dimitri.schrul@gmail.com>
2021-03-19 06:48:51 +0100

    add organization context with employees

--- src/test/kotlin/example/setup/Context.kt
+++ src/test/kotlin/example/setup/Context.kt
@@ -2,8 +2,15 @@ package example.setup
 
 import com.codefabrik.scenarios.Scenario
 import example.TestApplication
+import example.domain.model.employee.Employee
+import example.domain.model.employee.Role
 import example.domain.model.organization.Organization
 
 open class `test organization` : Scenario() {
     val testOrganization: Organization = TestApplication.organizationService.create("Test")
+}
+
+open class `organization with employees` : `test organization`() {
+    val supervisor = TestApplication.employeeService.create("supervisor", testOrganization.id, Role.SUPERVISOR)
+    val employee: Employee = TestApplication.employeeService.create("employee", testOrganization.id, Role.EMPLOYEE)
 }
